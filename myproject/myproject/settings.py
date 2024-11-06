8fd6ff56ad50455e24f26527e49d584c2947c05c
+ <+>
2024-11-06 20:36:37 +0300

    fix:fix a typo in settings file

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -30,10 +30,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bericks-backend.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bericks.onrender.com"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://bericks-backend.onrender.com/",
+    "https://bericks.onrender.com/",
 ]
 
 
