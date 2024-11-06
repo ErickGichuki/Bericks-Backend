83527cf4706c6a6357c89d0b9318f1910b668ac9
+ <+>
2024-11-06 14:55:36 +0300

    fix:set debug mode to False for prodcution purposes

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bericks-backend.onrender.com"]
 
